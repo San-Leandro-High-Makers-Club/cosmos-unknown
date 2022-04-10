081e02a24c925ee5981bf540271abad6f2538b65
Jahi Miller <millerjahi@gmail.com>
2022-04-10 15:40:53 -0700

    new f(x) resetMotors(), added code to control motors

--- robot.py
+++ robot.py
@@ -1,5 +1,5 @@
 #MOTOR CONTROllers
-DRIVE_CONTROLLER_ID = "6_1491370133845894324"
+DRIVE_CONTROLL_ID = "6_1491370133845894324"
 ARM_CONTROLLER_ID = "6_12577161366600381129"
 
 ##### CONTROLLS SECTION #####
@@ -15,12 +15,12 @@ ARM_L: bool = lambda: Gamepad.get_value("l_trigger")
 ##### MOTORS SECTION #####
 
 # TREAD MOTOR ID's
-TREAD_R = 'a'
-TREAD_L = 'b'
+TREAD_R = '_a'
+TREAD_L = '_b'
 
 # ARM MOTOR ID's
-ARM_ID_R = 'a'
-ARM_ID_L = 'b'
+ARM_ID_R = '_a'
+ARM_ID_L = '_b'
 
 #TREAD INVERSIONS
 TREAD_INVERT_L = False
@@ -40,5 +40,25 @@ def teleop_setup():
     pass
 
 def teleop_main():
-    if not JOYSTICK_RY() == 0:
-        pass
+    resetMotors()
+    if JOYSTICK_RY() != && JOYSTICK_LY() != 0:
+        Robot.set_value(DRIVE_CONTROLL_ID, "velocity"+TREAD_L, JOYSTICK_LY())
+        Robot.set_value(DRIVE_CONTROLL_ID, "velocity"+TREAD_R, JOYSTICK_RY())
+    elif JOYSTICK_RY() != 0 || JOYSTICK_LY() != 0:
+        device = DRIVE_CONTROLL_ID
+        if JOYSTICK_RY() != 0:
+            motor = "velocity"+TREAD_R
+            velocity = None
+        else:
+            motor = "velocity"+TREAD_L
+            velocity = None
+        Robot.set_value(device, motor, velocity)
+
+def resetMotors(device: str = None, id: str = None):
+    if not device:
+        Robot.set_value(DRIVE_CONTROLL_ID, "velocity"+TREAD_L, 0)
+        Robot.set_value(DRIVE_CONTROLL_ID, "velocity"+TREAD_R, 0)
+        Robot.set_value(ARM_CONTROLLER_ID, "velocity"+ARM_ID_R, 0)
+        Robot.set_value(ARM_CONTROLLER_ID, "velocity"+ARM_ID_R, 0)
+    else:
+        Robot.set_value(device, "velocity"+id, 0)
