d261832c4e8fb65af5e5acc66d1ec3a806785a70
Luke Peters <22827603+LukasPeterson@users.noreply.github.com>
2022-04-11 20:52:54 -0700

    Refactor and comment

--- robot.py
+++ robot.py
@@ -1,57 +1,74 @@
-##### MOTOR CONTROlLER SECTION #####
+#########################
+#                       #
+# Configuration         #
+#                       #
+#########################
 
-# CONTROLLER ID'S
+
+# Device IDs
 DRIVE_CONTROLLER_ID = "6_1491370133845894324"
 ARM_CONTROLLER_ID = "6_12577161366600381129"
 
-# DEADBAND RANGE'S
-DRIVE_DB_RANGE = 0.1
 
-##### CONTROLLS SECTION #####
+# How to determine the desired drive motor velocity during teleop mode
+def target_left_drive_motor_velocity():
+    velocity: float = Gamepad.get_value("joystick_left_y")
+    if abs(velocity) < 0.1:
+        velocity = 0
+    return velocity
 
-# reads a range from (-1.0, 1.0) --> (DOWN/LEFT, UP/RIGHT)
-JOYSTICK_RY: float = lambda: Gamepad.get_value("joystick_right_y")
-JOYSTICK_LY: float = lambda: Gamepad.get_value("joystick_left_y")
 
-# reads TRUE/FALSE value depending on if BTN is Pressed
+def target_right_drive_motor_velocity():
+    velocity: float = Gamepad.get_value("joystick_right_y")
+    if abs(velocity) < 0.1:
+        velocity = 0
+    return velocity
 
+
+# How to determine the desired arm motor velocity during teleop mode
 ARM_UR: bool = lambda: Gamepad.get_value("l_bumper")
 ARM_UL: bool = lambda: Gamepad.get_value("r_bumper")
 
 ARM_DR: bool = lambda: Gamepad.get_value("r_trigger")
 ARM_DL: bool = lambda: Gamepad.get_value("l_trigger")
 
-##### MOTORS SECTION #####
+# Which drive motor (a or b) is attached to the right and left wheels
+L_DRIVE_MOTOR = 'a'
+R_DRIVE_MOTOR = 'b'
 
-# TREAD MOTOR ID's
-TREAD_ID_R = '_b'
-TREAD_ID_L = '_a'
+# Which arm motor (a or b) is attached to the base of each arm
+L_ARM_MOTOR = 'b'
+R_ARM_MOTOR = 'a'
 
-# ARM MOTOR ID's
-ARM_ID_R = '_a'
-ARM_ID_L = '_b'
+# Whether the direction of the motors should be inverted
+INVERT_L_DRIVE_MOTOR = False
+INVERT_R_DRIVE_MOTOR = True
+INVERT_L_ARM_MOTOR = False
+INVERT_R_ARM_MOTOR = False
 
-# TREAD + ARM MOTOR INVERSIONS
-TREAD_INVERT_L = False
-TREAD_INVERT_R = True
-ARM_INVERT = False
-
-# ARM MOTOR SPEED
+# Speed at which the arms should raise and lower
 ARM_SPEED = 1.0
 
 
-def autonomous_setup():
-    # sets the inverts for the individual motors on each controller
-    Robot.set_value(DRIVE_CONTROLLER_ID, "invert" + TREAD_ID_L, TREAD_INVERT_L)
-    Robot.set_value(DRIVE_CONTROLLER_ID, "invert" + TREAD_ID_R, TREAD_INVERT_R)
-    Robot.set_value(ARM_CONTROLLER_ID, "invert" + ARM_ID_L, ARM_INVERT)
-    Robot.set_value(ARM_CONTROLLER_ID, "invert" + ARM_ID_R, ARM_INVERT)
-    
-    Robot.set_value(DRIVE_CONTROLLER_ID, "pid_enabled"+TREAD_ID_L, False)
-    Robot.set_value(DRIVE_CONTROLLER_ID, "pid_enabled"+TREAD_ID_R, False)
-    Robot.set_value(ARM_CONTROLLER_ID, "pid_enabled"+ARM_ID_L, False)
-    Robot.set_value(ARM_CONTROLLER_ID, "pid_enabled"+ARM_ID_R, False)
+#########################
+#                       #
+# End of configuration  #
+#                       #
+#########################
 
+
+def autonomous_setup():
+    # Set inversions for each motor
+    Robot.set_value(DRIVE_CONTROLLER_ID, "invert_" + L_DRIVE_MOTOR, INVERT_L_DRIVE_MOTOR)
+    Robot.set_value(DRIVE_CONTROLLER_ID, "invert_" + R_DRIVE_MOTOR, INVERT_R_DRIVE_MOTOR)
+    Robot.set_value(ARM_CONTROLLER_ID, "invert_" + L_ARM_MOTOR, INVERT_L_ARM_MOTOR)
+    Robot.set_value(ARM_CONTROLLER_ID, "invert_" + R_ARM_MOTOR, INVERT_R_ARM_MOTOR)
+
+    # Disable PID on each motor due to hardware issues
+    Robot.set_value(DRIVE_CONTROLLER_ID, "pid_enabled_" + L_DRIVE_MOTOR, False)
+    Robot.set_value(DRIVE_CONTROLLER_ID, "pid_enabled_" + R_DRIVE_MOTOR, False)
+    Robot.set_value(ARM_CONTROLLER_ID, "pid_enabled_" + L_ARM_MOTOR, False)
+    Robot.set_value(ARM_CONTROLLER_ID, "pid_enabled_" + R_ARM_MOTOR, False)
 
 
 def autonomous_main():
@@ -61,19 +78,13 @@ def autonomous_main():
 def teleop_setup():
     autonomous_setup()
 
-def teleop_main():
-    # updates the velocity param value of the motor, on condition TRUE
-    if abs(JOYSTICK_LY()) >= DRIVE_DB_RANGE:
-        Robot.set_value(DRIVE_CONTROLLER_ID, "velocity" + TREAD_ID_L, JOYSTICK_LY())
-    else:
-        Robot.set_value(DRIVE_CONTROLLER_ID, "velocity" + TREAD_ID_L, 0)
-    if abs(JOYSTICK_RY()) >= DRIVE_DB_RANGE:
-        Robot.set_value(DRIVE_CONTROLLER_ID, "velocity" + TREAD_ID_R, JOYSTICK_RY())
-    else:
-        Robot.set_value(DRIVE_CONTROLLER_ID, "velocity" + TREAD_ID_R, 0)
-        
-    Robot.set_value(ARM_CONTROLLER_ID, "velocity" + ARM_ID_L, ARM_SPEED*ARM_UL())
-    Robot.set_value(ARM_CONTROLLER_ID, "velocity" + ARM_ID_R, ARM_SPEED*ARM_UR())
-    Robot.set_value(ARM_CONTROLLER_ID, "velocity" + ARM_ID_L, ARM_SPEED*ARM_DL()*-1)
-    Robot.set_value(ARM_CONTROLLER_ID, "velocity" + ARM_ID_R, ARM_SPEED*ARM_DR()*-1)
 
+def teleop_main():
+    # Update velocity of each motor
+    Robot.set_value(DRIVE_CONTROLLER_ID, "velocity_" + L_DRIVE_MOTOR, target_left_drive_motor_velocity())
+    Robot.set_value(DRIVE_CONTROLLER_ID, "velocity_" + R_DRIVE_MOTOR, target_right_drive_motor_velocity())
+
+    Robot.set_value(ARM_CONTROLLER_ID, "velocity_" + L_ARM_MOTOR, ARM_SPEED * ARM_UL())
+    Robot.set_value(ARM_CONTROLLER_ID, "velocity_" + R_ARM_MOTOR, ARM_SPEED * ARM_UR())
+    Robot.set_value(ARM_CONTROLLER_ID, "velocity_" + L_ARM_MOTOR, ARM_SPEED * ARM_DL() * -1)
+    Robot.set_value(ARM_CONTROLLER_ID, "velocity_" + R_ARM_MOTOR, ARM_SPEED * ARM_DR() * -1)
