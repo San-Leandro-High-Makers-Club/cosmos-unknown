ad63fb9bde830d890e31332d8c48fefccbe1705c
Luke Peters <22827603+LukasPeterson@users.noreply.github.com>
2022-04-14 23:03:26 -0700

    Begin implementing line following and drive_forward()

--- robot.py
+++ robot.py
@@ -8,7 +8,8 @@
 # Device IDs
 DRIVE_CONTROLLER_ID = "6_1491370133845894324"
 ARM_CONTROLLER_ID = "6_12577161366600381129"
-LINE_FOLLOWER_ID = "2_3"
+CENTRE_LINE_FOLLOWER_ID = "2_3"
+LEADING_LINE_FOLLOWER_ID = "2_4"
 
 
 # How to determine the desired drive motor velocity during teleop mode
@@ -59,6 +60,15 @@ ARM_SPEED = 1.0
 # Speed at which the robot should drive during autonomous mode
 AUTONOMOUS_SPEED = 0.8
 
+# The minimum line follower reading that is considered to be on the tape
+ON_LINE_THRESHOLD = 0.18
+
+# The maximum line follower reading that is considered to be off the tape
+OFF_LINE_THRESHOLD = 0.12
+
+# The distance (as an encoder value) between the line follower sensors
+LINE_FOLLOWER_SEPARATION: 100  # TODO: calibrate
+
 
 #########################
 #                       #
@@ -69,7 +79,11 @@ AUTONOMOUS_SPEED = 0.8
 
 def autonomous_setup():
     teleop_setup()
-    
+
+    # Reset the drive encoders
+    Robot.set_value(DRIVE_CONTROLLER_ID, "enc_" + L_DRIVE_MOTOR, 0)
+    Robot.set_value(DRIVE_CONTROLLER_ID, "enc_" + R_DRIVE_MOTOR, 0)
+
     # Begin moving straight from the starting zone
     Robot.set_value(DRIVE_CONTROLLER_ID, "velocity_" + L_DRIVE_MOTOR, AUTONOMOUS_SPEED)
     Robot.set_value(DRIVE_CONTROLLER_ID, "velocity_" + R_DRIVE_MOTOR, AUTONOMOUS_SPEED)
@@ -77,6 +91,59 @@ def autonomous_setup():
 
 def autonomous_main():
     pass
+
+
+def drive_forward(distance: int, speed=AUTONOMOUS_SPEED, tolerance=20, stop=True) -> None:
+    """Drive in a straight line for the specified distance, decelerating near the end
+    
+    :param distance: the distance to drive (as an encoder value). If negative, drive in reverse.
+    :param speed: the speed at which to primarily drive. If extremely low, simply return immediately.
+    :param tolerance: the maximum distance (as an encoder value) by which it is acceptable to deviate from the specified
+        distance
+    :param stop: if True, come to rest after the specified distance has been travelled, and attempt adjustments if the
+        specified distance has been overshot
+    :return: None
+    """
+
+    speed = abs(speed)
+    if speed < 0.1:
+        return
+    tolerance = abs(tolerance)
+    if tolerance > abs(distance):
+        return
+    if distance < 0:
+        speed *= -1
+
+    initial_left_motor_position: int = Robot.get_value(DRIVE_CONTROLLER_ID, "enc_" + L_DRIVE_MOTOR)
+    initial_right_motor_position: int = Robot.get_value(DRIVE_CONTROLLER_ID, "enc_" + R_DRIVE_MOTOR)
+
+    def left_motor_distance_travelled():
+        return Robot.get_value(DRIVE_CONTROLLER_ID, "enc_" + L_DRIVE_MOTOR) - initial_left_motor_position
+
+    def right_motor_distance_travelled():
+        return Robot.get_value(DRIVE_CONTROLLER_ID, "enc_" + R_DRIVE_MOTOR) - initial_right_motor_position
+
+    def average_distance_travelled():
+        return int((left_motor_distance_travelled() + right_motor_distance_travelled()) / 2)
+
+    while abs(distance - average_distance_travelled()) > 1.5 * tolerance:
+        Robot.set_value(DRIVE_CONTROLLER_ID, "velocity_" + L_DRIVE_MOTOR, speed)
+        Robot.set_value(DRIVE_CONTROLLER_ID, "velocity_" + R_DRIVE_MOTOR, speed)
+
+    reduced_speed = 0.15
+    if distance < 0:
+        reduced_speed *= -1
+    while abs(distance - average_distance_travelled()) > tolerance:
+        Robot.set_value(DRIVE_CONTROLLER_ID, "velocity_" + L_DRIVE_MOTOR, reduced_speed)
+        Robot.set_value(DRIVE_CONTROLLER_ID, "velocity_" + R_DRIVE_MOTOR, reduced_speed)
+
+    if stop:
+        Robot.set_value(DRIVE_CONTROLLER_ID, "velocity_" + L_DRIVE_MOTOR, 0)
+        Robot.set_value(DRIVE_CONTROLLER_ID, "velocity_" + R_DRIVE_MOTOR, 0)
+        Robot.sleep(1)
+        if abs(distance - average_distance_travelled()) > tolerance:
+            adjustment_distance = -abs(distance - average_distance_travelled())
+            drive_forward(adjustment_distance, 0.5 * speed, tolerance, stop)
 
 
 def teleop_setup():
